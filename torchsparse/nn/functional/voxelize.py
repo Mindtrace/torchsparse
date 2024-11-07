357054c0e1e30bd8e097c0d2432dad7412d8bafa
YasserElHaddar16 <102835910+YasserElHaddar16@users.noreply.github.com>
2024-11-07 23:14:34 +0000

    bring back torch.cuda.amp

--- torchsparse/nn/functional/voxelize.py
+++ torchsparse/nn/functional/voxelize.py
@@ -1,6 +1,6 @@
 import torch
 from torch.autograd import Function
-from torch.amp import custom_bwd, custom_fwd
+from torch.cuda.amp import custom_bwd, custom_fwd
 
 import torchsparse.backend
 
@@ -10,7 +10,7 @@ __all__ = ['spvoxelize']
 class VoxelizeFunction(Function):
 
     @staticmethod
-    @custom_fwd(cast_inputs=torch.half, device_type='cuda')
+    @custom_fwd(cast_inputs=torch.half)
     def forward(ctx, feats: torch.Tensor, coords: torch.Tensor,
                 counts: torch.Tensor) -> torch.Tensor:
         feats = feats.contiguous()
@@ -31,7 +31,7 @@ class VoxelizeFunction(Function):
         return output
 
     @staticmethod
-    @custom_bwd(device_type='cuda')
+    @custom_bwd
     def backward(ctx, grad_output: torch.Tensor):
         coords, counts, input_size = ctx.for_backwards
         grad_output = grad_output.contiguous()
